03e4e24abeaf55d999d43244dfa5bc77dfb008b3
Daniel-Reyt <reytdaniel34@gmail.com>
2021-09-15 13:11:12 +0200

    dao client - Done

---
+++ orm/src/main/java/fr/lightning/orm/daos/ClientDao.java
@@ -0,0 +1,9 @@
+package fr.lightning.orm.daos;
+
+import fr.lightning.orm.models.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientDao extends JpaRepository<Client, Integer> {
+}
