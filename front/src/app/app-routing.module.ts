047c84611159efc54df308448a0022a74f1421b1
Daniel-Reyt <daniel-david.reyt@uha.fr>
2021-09-17 14:16:21 +0200

    init component de base angular

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,9 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FactureComponent } from './facture/facture.component';
 import {HomeComponent} from "./home/home.component";
+import { ChooseLoginComponent } from './login/choose-login/choose-login.component';
+import { LoginAvocatComponent } from './login/login-avocat/login-avocat.component';
+import { LoginClientComponent } from './login/login-client/login-client.component';
+import { RdvComponent } from './rdv/rdv.component';
+import { RegisterAvocatComponent } from './register/register-avocat/register-avocat.component';
+import { RegisterClientComponent } from './register/register-client/register-client.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: ChooseLoginComponent},
+  
+  {path: 'login-avocat', component: LoginAvocatComponent},
+  {path: 'login-client', component: LoginClientComponent},
+
+  {path: 'register-avocat', component: RegisterAvocatComponent},
+  {path: 'register-client', component: RegisterClientComponent},
+
+  {path: 'home', component: HomeComponent},
+
+  {path: 'facture', component: FactureComponent},
+  {path: 'rdv', component: RdvComponent}
 ];
 
 @NgModule({
---
+++ front/src/app/login/login-avocat/login-avocat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-avocat',
+  templateUrl: './login-avocat.component.html',
+  styleUrls: ['./login-avocat.component.css']
+})
+export class LoginAvocatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
